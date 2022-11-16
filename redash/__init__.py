18458067331548fbb80c67462d196d3ee0768190
Angelo Wolf <angelowolf21@gmail.com>
2022-11-16 12:13:30 -0300

    Update version to 10.1.1-frubana

--- redash/__init__.py
+++ redash/__init__.py
@@ -15,7 +15,7 @@ from .app import create_app  # noqa
 from .query_runner import import_query_runners
 from .destinations import import_destinations
 
-__version__ = "10.1.0"
+__version__ = "10.1.1-frubana" 
 
 
 if os.environ.get("REMOTE_DEBUG"):
