d28e4920c4f5bbb0f3a99c836cbc6e1c50604ea1
Angelo Wolf <angelowolf21@gmail.com>
2023-03-06 15:17:45 -0300

    v 10.3.2-frubana

--- redash/__init__.py
+++ redash/__init__.py
@@ -15,7 +15,7 @@ from .app import create_app  # noqa
 from .query_runner import import_query_runners
 from .destinations import import_destinations
 
-__version__ = "10.3.1-frubana" 
+__version__ = "10.3.2-frubana" 
 
 
 if os.environ.get("REMOTE_DEBUG"):
